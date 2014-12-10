5c2311faab6353f09d684ebb63038e86e9de7e0f
sarah <evangulyas@outlook.com>
2014-12-10 17:13:20 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+
+function arrayUnique(fromArray) {
+
+  var toArray = [];
+  var tempArray;
+
+  for (var i = 0; i < fromArray.length; i++) {
+
+    tempArray = fromArray.slice();
+    tempArray.splice(i, 1);
+
+    if (tempArray.indexOf(fromArray[i]) == -1) {
+      toArray.push(fromArray[i]);
+    }
+
+  }
+
+  return toArray;
+}
